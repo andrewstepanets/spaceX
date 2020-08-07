5892ea1459b7af832a0053021f84906a4115e7ec
Andrii Stepanets <ovrass@gmail.com>
2020-08-07 08:59:41 +0300

    Finished HTML CSS layout of promo site SpaceX

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Initialized VidBG plugin
+
+var instance = new vidbg('.video', {
+    mp4: 'video/world.mp4', // URL or relative path to MP4 video
+    webm: 'video/world.webm', // URL or relative path to webm video
+    poster: 'img/poster.jpg', // URL or relative path to fallback image
+    overlay: false, // Boolean to display the overlay or not
+});
+
+// Initialized Rellax plugin for parallax
+
+var rellax = new Rellax('.rocket');
+
+// Removed parallax when device screen are lower than 576 px
+
+if(document.body.clientWidth < 576) {
+    rellax.destroy();
+}
+
+// Initialized AOS Library
+
+AOS.init();
